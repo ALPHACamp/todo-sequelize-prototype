c708c0980674d7407cf1ad44f74143a7bb33c78e
Ellen Lee <ellen.lee@alphacamp.co>
2020-06-16 04:41:14 +0800

    fix format

--- migrations/20200615185643-create-todo.js
+++ migrations/20200615185643-create-todo.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.createTable('Todos', {
@@ -10,7 +10,7 @@ module.exports = {
       },
       name: {
         allowNull: false,
-        type: Sequelize.STRING,
+        type: Sequelize.STRING
       },
       isDone: {
         type: Sequelize.BOOLEAN,
@@ -24,9 +24,9 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
-    });
+    })
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Todos');
+    return queryInterface.dropTable('Todos')
   }
-};
+}
