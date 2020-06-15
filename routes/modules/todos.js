cc6e77d67a5f7ed12535c46cad44ced0b99e4273
Ellen Lee <ellen.lee@alphacamp.co>
2020-06-16 04:54:51 +0800

    add create todo function

---
+++ routes/modules/todos.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const router = express.Router()
+
+const db = require('../../models')
+const Todo = db.Todo
+
+router.get('/new', (req, res) => {
+  return res.render('new')
+})
+
+router.post('/', (req, res) => {
+  const UserId = req.user.id
+  const name = req.body.name
+
+  return Todo.create({ name, UserId })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+
+module.exports = router
