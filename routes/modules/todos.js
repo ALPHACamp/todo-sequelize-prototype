ddb006e621a53ff55810dc8adbf9ce76e5b74e1d
Ellen Lee <ellen.lee@alphacamp.co>
2020-06-16 05:14:21 +0800

    add delete todo function

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 const db = require('../../models')
 const Todo = db.Todo
+const User = db.User
 
 router.get('/new', (req, res) => {
   return res.render('new')
@@ -52,4 +53,15 @@ router.put('/:id', (req, res) => {
     .then(() => res.redirect(`/todos/${id}`))
     .catch(error => console.log(error))
 })
+
+router.delete('/:id', (req, res) => {
+  const UserId = req.user.id
+  const id = req.params.id
+
+  return Todo.findOne({ where: { id, UserId } })
+    .then(todo => todo.destroy())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 module.exports = router
